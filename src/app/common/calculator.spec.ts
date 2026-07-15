b1b54e2e0a79ba2056d210f30df7380305d1a289
Marcos Rivera <mjrivera@knowmadmood.com>
2026-07-15 13:29:52 +0200

    test: add ejemplos mockeos,  usando tambien mockClear, mockReset y mockRestore, con comentarios explicativos de cada uso.

--- src/app/common/calculator.spec.ts
+++ src/app/common/calculator.spec.ts
@@ -31,4 +31,78 @@ describe('Calculator', () => {
     expect(result).toBe(5);
   });
 
+  it('shows how to mock a function works', () => {
+    // Creamos un mock para la funcion add del objeto calculator. Que devuelva siempre 5, sin importar los parametros que reciba.
+    const addMock = vi.spyOn(calculator, 'add').mockReturnValue(5);
+    // const addMock = vi.spyOn(calculator, 'add').mockImplementation((a, b) => a * b);
+    const result = calculator.add(2, 3);
+    expect(addMock).toHaveBeenCalled();
+    expect(addMock).toHaveBeenCalledOnce();
+    expect(addMock).toHaveBeenCalledWith(2, 3);
+    expect(result).toBe(5);
+  });
+
+  it('shows how a pure mock works', () => {
+    // Creamos un mock puro, sin poder espiar las llamadas. Que devuelva siempre 55, sin importar los parametros que reciba.
+    // inicialmente no estamos espiando la funcion add del objeto calculator, sino que estamos creando un mock independiente.
+    const addMock = vi.fn().mockReturnValue(55);
+    const result = addMock(2, 3);
+    expect(result).toBe(55);
+
+    // Moqueamos la funcion add del objeto calculator con nuestro mock, para que cuando se llame a calculator.add, se ejecute nuestro mock.
+    vi.spyOn(calculator, 'add').mockImplementation(addMock);
+    const result2 = calculator.add(2, 3);
+    expect(result2).toBe(55);
+  });
+
+  it('shows how mock clearing works', () => {
+    // Creamos un spy para la funcion add del objeto calculator.
+    const spy = vi.spyOn(calculator, 'add');
+    const result = calculator.add(2, 3);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenCalledWith(2, 3);
+    expect(result).toBe(5);
+
+    spy.mockClear(); // sin esto, el contador de llamadas se acumula, por lo que si llamamos a calculator.add otra vez, el contador de llamadas sera 2. Ver más abajo.
+
+    const result2 = calculator.add(5, 5);
+    expect(spy).toHaveBeenCalledOnce(); // Gracias al mockClear, el contador de llamadas se resetea, por lo que ahora solo se ha llamado una vez.
+    expect(spy).toHaveBeenCalledWith(5, 5);
+    expect(result2).toBe(10);
+
+  });
+
+  it('shows how mockReset works for pure mocks', () => {
+    // Creamos un mock puro, sin poder espiar las llamadas. Que devuelva siempre 10, sin importar los parametros que reciba.
+    // inicialmente no estamos espiando la funcion add del objeto calculator, sino que estamos creando un mock independiente.
+    const addMock = vi.fn().mockReturnValue(10);
+    const result = addMock(5, 5);
+    expect(result).toBe(10);
+    expect(addMock).toHaveBeenCalledOnce();
+    expect(addMock).toHaveBeenCalledWith(5, 5);
+
+    addMock.mockReset(); // sin esto, el contador de llamadas se acumula, por lo que si llamamos a calculator.add otra vez, el contador de llamadas sera 2. Ver más abajo.
+
+    const result2 = addMock(5, 5);
+    expect(result2).toBe(undefined); // debido al mockReset, además de hacer lo mismo que el clear, el contador de llamadas se resetea, hace que el mock ya no tenga un valor de retorno definido, por lo que devuelve undefined (ya que elimina el comportamiento de mockReturnValue(10) ).
+    expect(addMock).toHaveBeenCalledOnce();
+    expect(addMock).toHaveBeenCalledWith(5, 5);
+  });
+
+  it('shows how mockRestore() works', () => {
+    // Creamos un spy para la funcion add del objeto calculator.
+    const spy = vi.spyOn(calculator, 'add');
+    const result = calculator.add(2, 3);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(result).toBe(5);
+
+    spy.mockRestore();// esto hace que el spy deje de espiar la funcion add del objeto calculator, por lo que ahora calculator.add vuelve a su implementacion original. Es decir es como eliminar el vi.spyOn(calculator, 'add');
+
+    const result2 = calculator.add(5, 5);
+    expect(spy).toHaveBeenCalledTimes(0); // Gracias al mockRestore, el contador de llamadas se resetea, porque elimina el spyOn... por lo que ahora se llama 0 veces.
+    expect(result2).toBe(10);
+
+  });
+
+
 });

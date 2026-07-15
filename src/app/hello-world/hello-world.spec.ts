74557579cb975cd7d0e9783c736d303a648a3fee
Marcos Rivera <mjrivera@knowmadmood.com>
2026-07-15 15:03:34 +0200

    fix: correccion del selector en el test de hello-world

---
+++ src/app/hello-world/hello-world.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { HelloWorld } from "./hello-world";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+
+describe.only('HelloWorld', () => {
+
+  let fixture: ComponentFixture<HelloWorld>;
+  let debugElement: DebugElement;
+  let el: HTMLElement;
+  let component: HelloWorld;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HelloWorld]
+    }).compileComponents();
+    TestBed.createComponent(HelloWorld);
+    fixture = TestBed.createComponent(HelloWorld);
+    debugElement = fixture.debugElement;
+    el = debugElement.nativeElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should display the message', () => {
+    const h1 = el.querySelector('h1');
+    expect(h1).toBeDefined();
+    expect(h1?.textContent).toEqual(component.message);
+  });
+
+});

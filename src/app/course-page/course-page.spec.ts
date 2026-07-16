d17609cbecc8d6aeb00cacd129e799001da9286a
Marcos Rivera <mjrivera@knowmadmood.com>
2026-07-16 12:45:49 +0200

    test: Test de carga la primera pagina, en CoursePage component. Mockeado el servicio, indicando que debe devolver y pero un test válido de que se ha llamado con los parametros que debiera

--- src/app/course-page/course-page.spec.ts
+++ src/app/course-page/course-page.spec.ts
@@ -1,11 +1,14 @@
-import {beforeEach, describe, vi} from 'vitest';
+import {beforeEach, describe, expect, it, vi} from 'vitest';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {DebugElement} from '@angular/core';
-import {MOCK_COURSES} from '../testing/testing-data';
+import {getMockLessonsPage, MOCK_COURSES} from '../testing/testing-data';
 import {CoursesService} from '../services/courses.service';
 import {CoursePage} from './course-page';
 import {ActivatedRoute} from '@angular/router';
+import {getTextContent} from "../testing/testing-utils";
 
+
+const FIRST_PAGE = getMockLessonsPage(1, '', 'asc', 0, 3);
 
 describe('CoursePage', () => {
   let component: CoursePage;
@@ -40,6 +43,23 @@ describe('CoursePage', () => {
     component = fixture.componentInstance;
 
   });
+
+  it('should load lessons on init', async () => {
+
+    mockCoursesService.findLessons.mockReturnValueOnce(FIRST_PAGE); // llamará a la función del servicio, pero como es un mock, no hace nada y devuelve lo que le digamos.
+
+    await fixture.whenStable(); // Esperar a que todas las tareas asíncronas se completen antes de continuar con las afirmaciones
+
+    expect(mockCoursesService.findLessons).toHaveBeenLastCalledWith(1, '', "asc", 0, 3); // Verificar que se haya llamado al servicio con los parámetros correctos
+
+    const lessons = getTextContent(de, "tbody tr td.description-cell");
+    expect(lessons).toHaveLength(3);
+    expect(lessons[0]).toBe("Lesson 1");
+    expect(lessons[1]).toBe("Lesson 2");
+    expect(lessons[2]).toBe("Lesson 3");
+
+  });
+
 
 });
 
